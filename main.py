e2692b19c6f95af08a9b4285e64b17e6319cdf80
joseph54287 <76684755+joseph54287@users.noreply.github.com>
2026-08-10 03:13:42 +0800

    標記代墊送出修正版

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import requests
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-APP_RELEASE = "2026-08-09-expense-v15"
+APP_RELEASE = "2026-08-10-expense-v16-submit-fix"
 import external_case
 LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET", "")
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN", "")
