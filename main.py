325e84cb2185c0be99b11f1d8e7d8f43ec730496
joseph54287 <76684755+joseph54287@users.noreply.github.com>
2026-08-09 23:30:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import requests
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-APP_RELEASE = "2026-08-09-expense-v15"
+APP_RELEASE = "2026-08-09-expense-v16-text-image-link"
 import external_case
 LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET", "")
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN", "")
